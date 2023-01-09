4a729a521e1ab24976b7d95c8d9a157f8766438f
edugerez <selmaredugerez@gmail.com>
2023-01-09 03:02:22 -0300

    portal home page activities

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+// Select the HTML element to manipulate
+const date = document.querySelector("#date");
+const message = document.querySelector("#emessage");
+
+//  method with options
+try {
+	const options = {
+	
+		day: "numeric",
+		month: "short",
+		year: "numeric"
+	};
+	date.innerHTML = `<span class="highlight"> ${new Date().toLocaleDateString("en-US",  options)}</span>!`;
+} catch (e) {
+	console.log("Error with code or your browser does not support Locale");
+}
+
+
